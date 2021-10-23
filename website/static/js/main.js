760eb4429ba72a746826bf64cff402f8387e920a
AliakbarSu <aliakbar.su@hotmail.com>
2021-10-23 20:58:08 +1300

    Add message send function

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,3 +1,16 @@
+let messageURL = 'http://127.0.0.1:5000/message'
+
+function sendMessage(payload) {
+  $.post(messageURL, payload, function (data, status) {})
+  addMessage(payload.message)
+}
+
+function addMessage(message, isBot) {
+  let classes = isBot ? 'single_message single_message_bot' : 'single_message'
+  let txt = $('<p></p>').text(message).addClass(classes)
+  $('#message_box').append(txt)
+}
+
 $.when($.ready).then(function () {
   $('#modal_close').bind('click', function () {
     $('#backdrop').hide()
@@ -7,5 +20,13 @@ $.when($.ready).then(function () {
   $('#chatNow').bind('click', function () {
     $('#backdrop').show()
     $('#modal').show()
+  })
+
+  $('#chat_form').on('submit', function (e) {
+    e.preventDefault()
+    let message = $('#textarea').val()
+    sendMessage({ message })
+    $('#backdrop').hide()
+    $('#modal').hide()
   })
 })
