bf5fe15f83ab65a6fd7b7249e6a39ff6fe3f7151
C0D3Crafter <70412835+C0D3Crafter@users.noreply.github.com>
2021-10-24 13:48:58 +1300

    Update scripture_api.py

--- BMIA/models/scripture_api.py
+++ BMIA/models/scripture_api.py
@@ -1,6 +1,4 @@
-import json # TEMP - Debug
 import os, requests
-from http import client
 
 class scripture_api:
     # Constructor
@@ -49,7 +47,8 @@ class scripture_api:
         HEADERS = {'api-key': self.api_key}
         return requests.get(url=URL, headers=HEADERS).json()
 
-    # Lists books of specified bible
+    # Lists books of the specified bible
+    #   [bible_id] The bible you want the books from
     def list_books(self, bible_id):
         self.__check_key()
 
@@ -58,27 +57,25 @@ class scripture_api:
         HEADERS = {'api-key': self.api_key}
         return requests.get(url=URL, headers=HEADERS).json()
 
-    # TEMP - Add docs
-    # Searches bible for relavant verses
-    #   [query] # comment about wildcards and only one word
-    #   [limit]
-    #   [fuzziness] # Explain fuzziness values
+    # Searches bible for verses that match query
+    #   [bible_id] The bible you want to search in
+    #   [query] Wildcards are '?' for single a character and '*' for multiple characters
+    #   [limit] Between 1-100
+    #   [fuzziness] Accounts for misspellings. Options are 'AUTO', '0', '1' and '2'
     def search(self, bible_id, query, limit = 10, fuzziness = "AUTO"):
         self.__check_key()
 
         # Set query
-        if (len(query.split(' ')) != 1):
-            raise ValueError(f"Error, Query can only contain one word! (Contains {len(query.split(' '))} words)")
-        elif (query == ""):
+        if (query == ""):
             raise ValueError(f"Error, Query cannot be empty!")
 
-        # Set limit (Between 1-100)
+        # Set limit
         if (limit < 1):
             raise ValueError(f"Error, Limit cannot be less than 0! ('{limit}')")
         elif (limit > 100):
             raise ValueError(f"Error, Limit cannot be more than 100! ('{limit}')")
 
-        # Set fuzziness (Accounts for misspellings)
+        # Set fuzziness
         if not (fuzziness == "AUTO" or fuzziness == "0" or fuzziness == "1" or fuzziness == "2"):
             raise ValueError(f"Error, Invalid fuzziness value! ('{fuzziness}') (Options are 'AUTO', '0', '1' and '2')")
 
@@ -86,36 +83,3 @@ class scripture_api:
         URL = f"https://api.scripture.api.bible/v1/bibles/{bible_id}/search?query={query}&limit={limit}&fuzziness={fuzziness}"
         HEADERS = {'api-key': self.api_key}
         return requests.get(url=URL, headers=HEADERS).json()
-
-# TEMP - Testing
-def main():
-    os.chdir("models/") # Change directory to "models/"
-
-    # Retrieve API key
-    api_key = ""
-    with open("../../website/models/api_key.txt", "r") as api_key_file:
-        lines =  api_key_file.readlines() # Read file
-
-        # Validate file content
-        if (len(lines) == 0):
-            raise Exception(f"Error, API key not found! (\"{os.getcwd()}/api_key.txt\")")
-        elif (len(lines) > 1):
-            if (lines[1] != ""):
-                raise  Exception(f"Error, API key cannot be across multiple lines! (\"{os.getcwd()}/api_key.txt\")")
-
-        api_key = lines[0] # Access API key
-    print(f"API key found! ('{api_key}')") # TEMP - Debug
-
-
-    # TEMP - Debug
-    api_test = scripture_api()
-    api_test.authenticate(api_key)
-    bible_id = '65eec8e0b60e656b-01' # Free Bible Version
-    # TEMP - Debug
-    # response = api_test.list_bibles("eng")
-    # response = api_test.list_books(bible_id)
-    response = api_test.list_books(bible_id)
-    print(f"Response:\n{json.dumps(response, indent=4, sort_keys=True)}")
-
-if __name__ == '__main__':
-    main()
